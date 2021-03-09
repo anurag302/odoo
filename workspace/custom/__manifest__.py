29ca32883afc0c29eafe4f410ec08b6a7b4566e5
anurag302 <anurag310510@gmail.com>
2021-03-09 18:52:27 +0530

    odoo project

---
+++ workspace/custom/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+	
+'name': 'School',
+	
+'version': '12.0.1.0.0',
+	
+'summary': 'Record Student Information',
+	
+'category': 'Tools',
+	
+'author': 'anurag',
+	
+'maintainer': 'anurag',
+	
+'company': 'Anurag',
+	
+'website': 'canurag570@gmail.com',
+	
+'depends': ['base'],
+	
+'data': [
+    'views/student_view.xml',
+    'security/ir.model.access.csv'
+	
+],
+	
+'images': [],
+	
+'license': 'AGPL-3',
+	
+'installable': True,
+	
+'application': False,
+	
+'auto_install': False,
+}
